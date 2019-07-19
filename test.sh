25822a3fcf95897a1c039d0ba5eda908fd280b84
Kiran Raju <53047603+kiranraju88@users.noreply.github.com>
2019-07-19 23:24:22 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,27 +3,30 @@
 # Example for using User Defined Variables with JMeter
 # These will be substituted in JMX test script
 
-export Threads="1"
-
-T_DIR=/
+ARG Threads="1"
+ARG JmxFile="1CApp"
+T_DIR=/home/ec2-user/
 
 # Reporting dir: start fresh
+# set Folder for report
 R_DIR=${T_DIR}/report
+#Delete Report folder. better undestanding about "/dev/null 2>&1" https://stackoverflow.com/questions/10508843/what-is-dev-null-21
 rm -rf ${R_DIR} > /dev/null 2>&1
+# MAKE REPORT FOLDER 
 mkdir -p ${R_DIR}
 
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
-
+#Run./run file to trigger build
 ./run.sh -Dlog_level.jmeter=DEBUG \
 	-Jthreads=${Threads} \
-	-n -t ${T_DIR}/1CApp.jmx -l ${T_DIR}/1CApp.jtl -j ${T_DIR}/jmeter.log \
+	-n -t ${T_DIR}/${JmxFile}.jmx -l ${R_DIR}/${JmxFile}.jtl -j ${R_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
-cat ${T_DIR}/jmeter.log
+cat ${R_DIR}/jmeter.log
 
 echo "==== Raw Test Report ===="
-cat ${T_DIR}/1CApp.jtl
+cat ${R_DIR}/${JmxFile}.jtl
 
 echo "==== HTML Test Report ===="
-echo "See HTML test report in ${R_DIR}/1CApp.html"
+echo "See HTML test report in ${R_DIR}/${JmxFile}.html"
