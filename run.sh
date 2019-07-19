e08489dbd525b09be9c1e321324cbdd07305495c
Kiran Raju <53047603+kiranraju88@users.noreply.github.com>
2019-07-19 23:08:06 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@
 JMETER_VERSION="5.1.1"
 NAME="jmeter"
 IMAGE="jmeter:${JMETER_VERSION}"
-
+WORK_DIR="/home/ec2-user"
 # Finally run
 sudo docker stop ${NAME} > /dev/null 2>&1
 sudo docker rm ${NAME} > /dev/null 2>&1
-sudo docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
+#sudo docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
+sudo docker run --name ${NAME} -i -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR} ${IMAGE}
