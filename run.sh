4b0e02fcf98424e9796e87729e945fe54ae2c74f
Kiran Raju <53047603+kiranraju88@users.noreply.github.com>
2019-07-23 12:49:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ JMETER_VERSION="5.1.1"
 NAME="jmeter"
 IMAGE="jmeter:${JMETER_VERSION}"
 WORK_DIR="/home/ec2-user"
-# Finally run
+# Finally run 
+# start a container in detached mode, you use -d=true or just -d option.
 sudo docker stop ${NAME} > /dev/null 2>&1
+#https://unix.stackexchange.com/questions/163352/what-does-dev-null-21-mean-in-this-article-of-crontab-basics
 sudo docker rm ${NAME} > /dev/null 2>&1
-#sudo docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
-sudo docker run --name ${NAME} -i -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR} ${IMAGE}
+sudo docker run -d --name ${NAME} -i -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR} ${IMAGE}
