9cc055b5d0aa2719ac61a04bf970a16f7f45b6ec
Kiran Raju <53047603+kiranraju88@users.noreply.github.com>
2019-07-25 11:40:39 +0000

    Add files via upload

--- executetest.sh
+++ executetest.sh
@@ -3,30 +3,29 @@
 # Example for using User Defined Variables with JMeter
 # These will be substituted in JMX test script
 
-ARG Threads="1"
-ARG JmxFile="1CApp"
-T_DIR=/home/ec2-user/
+export Threads="1"
+#Duration in seconds
+export Duration="120"
+
+T_DIR=/home/ec2-user
 
 # Reporting dir: start fresh
-# set Folder for report
 R_DIR=${T_DIR}/report
-#Delete Report folder. better undestanding about "/dev/null 2>&1" https://stackoverflow.com/questions/10508843/what-is-dev-null-21
-rm -rf ${R_DIR} > /dev/null 2>&1
-# MAKE REPORT FOLDER 
+sudo rm -rf ${R_DIR} > /dev/null 2>&1
 mkdir -p ${R_DIR}
 
-/bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
-#Run./run file to trigger build
+/bin/rm -f ${T_DIR}/1CApp.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
+
 ./run.sh -Dlog_level.jmeter=DEBUG \
-	-Jthreads=${Threads} \
-	-n -t ${T_DIR}/${JmxFile}.jmx -l ${R_DIR}/${JmxFile}.jtl -j ${R_DIR}/jmeter.log \
+	-Jthreads=${Threads} -Jduration=${Duration} \
+	-n -t ${T_DIR}/1CApp.jmx -l ${T_DIR}/1CApp.jtl -j ${T_DIR}/jmeter.log \
 	-e -o ${R_DIR}
 
 echo "==== jmeter.log ===="
-cat ${R_DIR}/jmeter.log
+cat ${T_DIR}/jmeter.log
 
 echo "==== Raw Test Report ===="
-cat ${R_DIR}/${JmxFile}.jtl
+cat ${T_DIR}/1CApp.jtl
 
 echo "==== HTML Test Report ===="
-echo "See HTML test report in ${R_DIR}/${JmxFile}.html"
+echo "See HTML test report in ${R_DIR}/1CApp.html"
--- run.sh
+++ run.sh
@@ -4,10 +4,8 @@
 JMETER_VERSION="5.1.1"
 NAME="jmeter"
 IMAGE="jmeter:${JMETER_VERSION}"
-WORK_DIR="/home/ec2-user"
-# Finally run 
-# start a container in detached mode, you use -d=true or just -d option.
+
+# Finally run
 sudo docker stop ${NAME} > /dev/null 2>&1
-#https://unix.stackexchange.com/questions/163352/what-does-dev-null-21-mean-in-this-article-of-crontab-basics
 sudo docker rm ${NAME} > /dev/null 2>&1
-sudo docker run -d --name ${NAME} -i -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR} ${IMAGE}
+sudo docker run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
